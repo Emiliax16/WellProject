b6cfe1d461e8876f6acb2e0a230b7ec7bb7d3c71
jpvergaralb <jpvergaralb@uc.cl>
2024-04-16 00:24:24 -0400

    chore: move routes to their folders & add readme helper

---
+++ API/src/controllers/placeholder.controller.js
@@ -0,0 +1,9 @@
+const placeholderController = (req, res) => {
+    res.json({
+      data: 'This is a placeholder'
+    });
+}
+
+module.exports = {
+    placeholderController
+}
---
+++ API/src/middlewares/placeholder.middleware.js
@@ -0,0 +1,8 @@
+const requestLogger = (req, res, next) => {
+    console.log(`Received a ${req.method} request to ${req.path}`);
+    next();
+}
+
+module.exports = {
+    requestLogger
+}
