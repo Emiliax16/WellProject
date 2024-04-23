8ace7b9bf0da82b949d188ed22f52effaf72f434
jpvergaralb <jpvergaralb@uc.cl>
2024-04-23 02:08:22 -0400

    feat: agregar middleware de manejo de errores

---
+++ API/src/middlewares/error.middleware.js
@@ -0,0 +1,27 @@
+const { ValidationError, ConnectionError, DatabaseError } = require('sequelize');
+const ErrorHandler = require('../utils/error.util');
+
+const errorHandler = (err, req, res, next) => {
+  console.log(err)
+  console.log(`Error 🚨 | ${err.message} while sending a ${req.method} to ${req.originalUrl}`);
+  if (err instanceof ValidationError) {
+    const messages = err.errors.map((error) => error.message);
+    return res.status(400).json({ errors: messages });
+  }
+
+  if (err instanceof ErrorHandler) {
+    return res.status(err.statusCode).json({ error: err.message });
+  }
+
+  if (err instanceof ConnectionError) {
+    return res.status(500).json({ error: err.message });
+  }
+
+  if (err instanceof DatabaseError) {
+    return res.status(400).json({ error: err.message });
+  }
+
+  return res.status(500).json({ error: err.message });
+};
+
+module.exports = errorHandler
