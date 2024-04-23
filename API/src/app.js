5ebbbf237d6f1d5c1bb6193c1466a6318375bd35
jpvergaralb <jpvergaralb@uc.cl>
2024-04-23 02:05:34 -0400

    feat: agregar middlewares y ruta para endpoints de user
    - `user`: se agrega la ruta para `user`
    - `middlewares`: se agrega un middleware de manejo de errores
    - se refactorizo un poco el import de algunos archivos

--- API/src/app.js
+++ API/src/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const {
-  requestLogger
-} = require('./middlewares/placeholder.middleware');
+const requestLogger = require('./middlewares/placeholder.middleware');
+const errorHandler = require('./middlewares/error.middleware');
 const placeholderRoute = require('./routes/placeholder.route');
 const wellRoute = require('./routes/well.route');
 const wellDataRoute = require('./routes/wellData.route');
+const userRoute = require('./routes/user.route');
 
 // App initialization
 const app = express();
@@ -14,8 +14,12 @@ app.use(express.json());
 app.use(requestLogger);
 
 // Rutas
+app.use(userRoute);
 app.use(placeholderRoute)
 app.use(wellRoute);
 app.use(wellDataRoute);
+
+// Middleware de manejo de errores
+app.use(errorHandler);
 
 module.exports = app;
