1a9fbc52cf8278cf26c69ac10d959c7cb1db91ac
jpvergaralb <jpvergaralb@uc.cl>
2024-04-26 23:53:08 -0400

    chore: agregar dependencia cors para peticiones de recursos con diferentes origines

--- API/src/app.js
+++ API/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const requestLogger = require('./middlewares/placeholder.middleware');
 const errorHandler = require('./middlewares/error.middleware');
 const placeholderRoute = require('./routes/placeholder.route');
@@ -10,6 +11,7 @@ const userRoute = require('./routes/user.route');
 const app = express();
 
 // Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 
