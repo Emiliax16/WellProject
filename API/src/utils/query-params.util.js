c039be88880ff256d207dadecf154075dd2de916
jpvergaralb <jpvergaralb@uc.cl>
2024-05-30 14:31:35 -0400

    fix: corregir error de valores por defecto entre limit y offset

--- API/src/utils/query-params.util.js
+++ API/src/utils/query-params.util.js
@@ -1,7 +1,7 @@
 const getPaginationParameters = (query) => {
-  const limit = query.size ? parseInt(query.size, 10) : 1;
-  const page = query.page ? parseInt(query.page, 10) : 25;
-  const offset = (page - 1) * limit;
+  const limit = query.size ? parseInt(query.size, 10) : 25;
+  const page = query.page ? parseInt(query.page, 10) : 0;
+  const offset = (page) * limit;
   return { limit, offset };
 };
 
