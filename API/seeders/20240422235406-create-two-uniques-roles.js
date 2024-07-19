c9a967db09691cebf02b522e53940c571928eeba
jpvergaralb <jpvergaralb@uc.cl>
2024-07-18 22:25:40 -0400

    chore: agregar nuevo rol de company en la seed de roles

--- API/seeders/20240422235406-create-two-uniques-roles.js
+++ API/seeders/20240422235406-create-two-uniques-roles.js
@@ -5,19 +5,32 @@ module.exports = {
   async up (queryInterface, Sequelize) {
    await queryInterface.bulkInsert('roles', [
     {
+      id: 1,
       type: 'admin',
       description: 'Role for admin users, can view and edit all data',
       isAdmin: true,
+      isCompany: false,
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
+      id: 2,
       type: 'normal',
       description: 'Role for normal users, can only view and edit their own data',
       isAdmin: false,
+      isCompany: false, 
       createdAt: new Date(),
       updatedAt: new Date()
-    }]);
+    },
+    {
+      id: 3,
+      description: 'Role for company users, can view and edit all data',
+      isAdmin: false,
+      isCompany: true,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ]);
   },
   
   async down (queryInterface, Sequelize) {
