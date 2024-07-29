ff5c68b1d7c519cf0517c367f05d645e2d8efbeb
Emiliax16 <efpg1608@uc.cl>
2024-07-29 02:17:17 -0400

    refactor: cambiar forma en la que se entregan roles permitidos a la validación de las rutas

--- API/src/routes/client.route.js
+++ API/src/routes/client.route.js
@@ -14,17 +14,24 @@ const {
   createClientWell,
   addDataToClientWell,
 } = require('../controllers/client.controller');
+const {
+  AllRoles,
+  AdminAndCompany,
+  Admin
+} = require('../utils/allowed-roles.util');
 
 const router = express.Router();
 
-router.get('/clients', authMiddleware('normal', 'admin'), getAllClients);
-router.get('/clients/:id/wells', authMiddleware('normal', 'admin'), getClientWells);
-router.get('/clients/:id/wells/:code/data', authMiddleware('normal', 'admin'), getWellData);
-router.put('/clients/:id/edit', authMiddleware('normal', 'admin'), checkTroll(), validateParams(editDataOfClient), editClient);
-router.post('/clients/:id/wells/create', authMiddleware('normal', 'admin'), validateParams(createWell), createClientWell);
-router.post('/clients/:id/wells/:code/add', authMiddleware('normal', 'admin'), validateParams(addDataToWell), addDataToClientWell);
-router.delete('/clients/:id/delete', authMiddleware('normal', 'admin'), deleteClient);
-router.delete('/clients/:id/wells/:code/delete', authMiddleware('normal', 'admin'), deleteClientWell);
-router.put('/clients/:id/wells/:code/edit', authMiddleware('admin'), validateParams(editDataOfWell), editClientWell);
+// ! Ojo la mayoria no deberia dejar pasar a un user regular, pero quiza eso es tarea de otro pr
+// Por ahora lo dejare tal cuaal como estaba y simplemnte dejare pasar tambien a company
+router.get('/clients', authMiddleware(...AllRoles), getAllClients); 
+router.get('/clients/:id/wells', authMiddleware(...AllRoles), getClientWells);
+router.get('/clients/:id/wells/:code/data', authMiddleware(...AllRoles), getWellData);
+router.put('/clients/:id/edit', authMiddleware(...AllRoles), checkTroll(), validateParams(editDataOfClient), editClient);
+router.post('/clients/:id/wells/create', authMiddleware(...AllRoles), validateParams(createWell), createClientWell);
+router.post('/clients/:id/wells/:code/add', authMiddleware(...AllRoles), validateParams(addDataToWell), addDataToClientWell);
+router.delete('/clients/:id/delete', authMiddleware(...AllRoles), deleteClient);
+router.delete('/clients/:id/wells/:code/delete', authMiddleware(...AllRoles), deleteClientWell);
+router.put('/clients/:id/wells/:code/edit', authMiddleware(AdminAndCompany), validateParams(editDataOfWell), editClientWell);
 
 module.exports = router;
